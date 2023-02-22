bdb26e51ecb572decaaf6a7c3fdb00b3a0072c67
Aswinbabus <aswinbabu125@gmail.com>
2023-02-22 08:40:09 +0530

    22-02-2023 Completed...

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,69 @@
-﻿
-
-import com.sun.deploy.util.StringUtils;
-
-import java.util.*;
-
-
+﻿import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int val = solution.singleNonDuplicate(new int[]{1,3,3});
-        System.out.println(val);
+        System.out.println(solution.shipWithinDays(new int[]{1,2,3,4,5,6,7,8,9,10},5));
+        System.out.println(solution.shipWithinDays(new int[]{3,2,2,4,1,4},3));
+
     }
 }
 class Solution {
-    public int singleNonDuplicate(int[] nums) {
+    public int shipWithinDays(int[] weights, int maxDays) {
 
-        int n = nums.length;
-        int left = 0,right = n-1;
+        int minWeight = weights[0];
+        int maxWeight = Arrays.stream(weights).sum();
+        int minLoad = Integer.MAX_VALUE;
 
-        while( left <= right ) {
+        // Binary search minWeight to maxWeight to find the optimal load for the ship
 
-            int mid = left + (right - left) / 2;
+        int left = minWeight,right = maxWeight,mid;
+        while(left <= right) {
+
+            mid = left + (right - left) / 2;
+
+            int days = computeDaysToShip(weights,mid);
 
 
-            if(  !checkPrev(nums,mid) && !checkNext(nums,mid)  ) {
-                return nums[mid];
-            }
-            else if( (mid%2 ==0 && checkPrev(nums,mid)) || (mid%2!=0 && !checkPrev(nums,mid)) ) {
-                right = mid;
-            }
-            else {
+            if(days == -1 || days > maxDays) {
                 left = mid + 1;
             }
+            else {
 
+                minLoad = Math.min(minLoad,mid);
+                right = mid - 1;
 
+            }
 
         }
 
-        return nums[0];
-    }
-
-    private boolean checkNext(int[] nums, int mid) {
-
-        if( mid+1 >= nums.length) {
-            return false;
-        }
-
-        if( nums[mid+1] ==  nums[mid]) {
-            return true;
-        }
-
-        return false;
+        return minLoad;
 
     }
 
-    private boolean checkPrev(int[] nums, int mid) {
+    private int computeDaysToShip(int[] weights,int capacity) {
 
-        if( mid - 1 < 0) {
-            return false;
+        int currLoad = 0,days = 0;
+
+        for(int i = 0;i<weights.length;i++) {
+
+            if(currLoad == 0 && weights[i] > capacity) {
+                return -1;
+            }
+
+            currLoad += weights[i];
+
+            if(currLoad >= capacity) {
+                currLoad -= capacity;
+                days++;
+            }
+
         }
 
-        if( nums[mid-1] == nums[mid]) {
-            return true;
+        if(currLoad != 0) {
+            days++;
         }
 
-        return false;
+        return days;
+
     }
 }
